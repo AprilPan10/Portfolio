1979509450bd3aa155a40ec945f1f1a24bd76e81
AprilPan10 <63117386+AprilPan10@users.noreply.github.com>
2021-05-02 23:14:50 -0400

    changes

--- model/Database.php
+++ model/Database.php
@@ -1,9 +1,9 @@
 <?php
 namespace Portfolio\model;
 class Database{
-    private static $user = "shimxxju_April";
-    private static $pass = "kyU1Z@.u36@W";
-    private static $dsn = "mysql:host=localhost;dbname=shimxxju_contact_me";
+    private static $user = "";
+    private static $pass = "";
+    private static $dsn = "mysql:host=localhost;dbname=";
     private static $dbcon;
 
     private function __construct(){
